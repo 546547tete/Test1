4f1bafabd98c2b45e842150069747989867ebed1
tirigun <terigun@csii.com.cn>
2021-01-12 17:37:53 +0800

    new

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "666", Toast.LENGTH_SHORT).show();
+        String s="sdas";
     }
 }
