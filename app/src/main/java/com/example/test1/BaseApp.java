510aec87984ff7669722febc092779d42b0d1560
chaolemen-github <2863346378@qq.com>
2021-01-13 11:26:10 +0800

    baseApp

--- app/src/main/java/com/example/test1/BaseApp.java
+++ app/src/main/java/com/example/test1/BaseApp.java
@@ -7,8 +7,10 @@ import com.example.test1.db.DaoMaster;
 public class BaseApp extends Application {
 
     private static BaseApp baseApp;
-    private BaseApp() {
-        baseApp=this;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        baseApp = this;
     }
 
     public static BaseApp getBaseApp() {
