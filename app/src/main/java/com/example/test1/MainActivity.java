a5881051f54f0bce0fc26edf8a2452b08b080011
tirigun <terigun@csii.com.cn>
2021-01-13 09:59:35 +0800

    123456

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.test1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rcy;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "666", Toast.LENGTH_SHORT).show();
-        String s="sdas";
-        //
+        initView();
+    }
 
-
-
-        int a = 10;
+    private void initView() {
+        rcy = (RecyclerView) findViewById(R.id.rcy);
+        img = (ImageView) findViewById(R.id.img);
     }
 }
