26030c0451fe810771783b389942678a6ecf84db
chaolemen-github <2863346378@qq.com>
2021-01-13 13:54:08 +0800

    popup

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.test1;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+import android.widget.PopupWindow;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,31 +35,34 @@ public class MainActivity extends AppCompatActivity {
     private HomeAdapter homeAdapter;
     private List<BeanDao> all;
     private BeanDao dao1;
+    private int login_type = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initData();
+        SpUtil.setParam("login_type", login_type);
+        login_type++;
         initView();
+        initData();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.home_menu,menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_no:
                 List<BeanDao> queryAllno = DBHolper.getInstance().queryAll();
                 ArrayList<BeanDao> listno = new ArrayList<>();
                 for (BeanDao beanDao : queryAllno) {
-                    if (beanDao.getType().equals("1")){
+                    if (beanDao.getType().equals("1")) {
                         listno.add(beanDao);
-                       homeAdapter.setData(listno);
+                        homeAdapter.setData(listno);
                     }
                 }
                 break;
@@ -60,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
                 List<BeanDao> queryAllyes = DBHolper.getInstance().queryAll();
                 ArrayList<BeanDao> listyes = new ArrayList<>();
                 for (BeanDao beanDao : queryAllyes) {
-                    if (beanDao.getType().equals("2")){
+                    if (beanDao.getType().equals("2")) {
                         listyes.add(beanDao);
                         homeAdapter.setData(listyes);
                     }
@@ -70,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
                 List<BeanDao> alldelete = DBHolper.getInstance().queryAll();
                 List<BeanDao> beanDaosdelete = new ArrayList<>();
                 for (BeanDao beanDao : alldelete) {
-                    if (beanDao.getType().equals("3")){
+                    if (beanDao.getType().equals("3")) {
                         beanDaosdelete.add(beanDao);
                         homeAdapter.setData(beanDaosdelete);
-                    }else {
+                    } else {
 //                        BeanDao dao2 = new BeanDao((long) 999,"没有删除过的任务","3",false);
 //                        beanDaosdelete.add(dao2);
                         Toast.makeText(this, "没有删除过的任务", Toast.LENGTH_SHORT).show();
@@ -86,7 +96,7 @@ public class MainActivity extends AppCompatActivity {
                 List<BeanDao> all = DBHolper.getInstance().queryAll();
                 ArrayList<BeanDao> beanDaos = new ArrayList<>();
                 for (BeanDao beanDao : all) {
-                    if (!beanDao.getType().equals("3")){
+                    if (!beanDao.getType().equals("3")) {
                         beanDaos.add(beanDao);
                     }
                 }
@@ -97,26 +107,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        int type1 = (int) SpUtil.getParam("login_type", 0);
         Long aLong = Long.valueOf(0);
         List<String> arrayList = new ArrayList<>();
         arrayList.add("扫地");
         arrayList.add("洗衣服");
         arrayList.add("做饭");
         arrayList.add("洗澡");
-        for (int i = 0; i < arrayList.size(); i++) {
-            dao1 = new BeanDao((long) out,arrayList.get(i),"1",false);
-            DBHolper.getInstance().insert(dao1);
-            out++;
-        }
-//        BeanDao dao1 = new BeanDao(aLong,"扫地","1",false);
-        BeanDao dao2 = new BeanDao(aLong,"洗衣服","1",false);
-        BeanDao dao3 = new BeanDao(aLong,"做饭","1",false);
-        BeanDao dao4 = new BeanDao(aLong,"洗澡","1",false);
 
-//        DBHolper.getInstance().insert(dao2);
-//        DBHolper.getInstance().insert(dao3);
-//        DBHolper.getInstance().insert(dao4);
+//        if (type1 == 1) {
+//            for (int i = 0; i < arrayList.size(); i++) {
+//                dao1 = new BeanDao((long) out, arrayList.get(i), "1", false);
+//                DBHolper.getInstance().insert(dao1);
+//                out++;
+//            }
+//        }
         all = DBHolper.getInstance().queryAll();
+        homeAdapter.setData(all);
     }
 
     private void initView() {
@@ -125,8 +132,45 @@ public class MainActivity extends AppCompatActivity {
         rcy.setLayoutManager(new LinearLayoutManager(this));
         rcy.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         homeAdapter = new HomeAdapter(this);
-        homeAdapter.setData(all);
         rcy.setAdapter(homeAdapter);
         homeAdapter.notifyDataSetChanged();
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initPop();
+            }
+        });
+    }
+
+    private void initPop() {
+        View inflate = LayoutInflater.from(this).inflate(R.layout.item_popup, null);
+        final PopupWindow popupWindow = new PopupWindow(inflate, 600, 500);
+        popupWindow.setBackgroundDrawable(new ColorDrawable());
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.showAtLocation(inflate, Gravity.CENTER, 0, 0);
+
+        final EditText et_pop = inflate.findViewById(R.id.et_pop);
+        Button btn_ok = inflate.findViewById(R.id.btn_ok);
+        Button btn_no = inflate.findViewById(R.id.btn_no);
+
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String trim = et_pop.getText().toString().trim();
+                out++;
+                BeanDao beanDao = new BeanDao((long) out, trim, "1", false);
+                DBHolper.getInstance().insert(beanDao);
+                popupWindow.dismiss();
+            }
+        });
+        btn_no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+
     }
 }
